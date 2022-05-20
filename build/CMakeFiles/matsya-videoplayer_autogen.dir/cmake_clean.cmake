8027240f84cfa4024aec2d4791d3f798b01a981b
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-20 21:56:11 +0530

    ...

--- build/CMakeFiles/matsya-videoplayer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/matsya-videoplayer_autogen"
-  "matsya-videoplayer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/matsya-videoplayer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
